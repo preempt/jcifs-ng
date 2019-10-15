05686cd9bd0d22a7b722e99e2a16a83bc05b074c
Marina Simakov <msimakov@preempt.com>
2019-10-15 22:24:28 +0300

    SYS-15684: Add the spoolss interface

--- src/main/java/jcifs/dcerpc/DcerpcBinding.java
+++ src/main/java/jcifs/dcerpc/DcerpcBinding.java
@@ -20,15 +20,15 @@
 package jcifs.dcerpc;
 
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import jcifs.dcerpc.msrpc.lsarpc;
 import jcifs.dcerpc.msrpc.netdfs;
 import jcifs.dcerpc.msrpc.samr;
 import jcifs.dcerpc.msrpc.srvsvc;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
@@ -47,6 +47,7 @@ public class DcerpcBinding {
         INTERFACES.put("netlogon", "12345678-1234-abcd-ef00-01234567cffb:1.0");
         INTERFACES.put("wkssvc", "6BFFD098-A112-3610-9833-46C3F87E345A:1.0");
         INTERFACES.put("samr", "12345778-1234-ABCD-EF00-0123456789AC:1.0");
+        INTERFACES.put("spoolss", "12345678-1234-ABCD-EF00-0123456789AB:1.0");
     }
 
 
